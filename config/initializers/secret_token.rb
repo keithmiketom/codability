9b923630066dbaedf3240b112c509a309f50927d
Keith Michael Gregg <keithmichaelgregg@Keiths-MacBook-Pro.local>
2014-12-23 15:39:13 +0000

    initiated rails 3 repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Codability::Application.config.secret_token = '5f2341fef910dc9e9353a419fe42ba3c1428f66a501dcdf492f9255291db28bad87a787006e0ccbd7efb76356480df076bf4a7505306a202f2015fab05f33cf4'
